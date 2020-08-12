837cdaef9c2338011e95048299e8960a287e49f9
Luiz Amaral <alaluizamaral@gmail.com>
2020-08-12 16:08:12 -0300

    Finalização do curso 1

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,25 +1,31 @@
 class NegociacaoController {
-
   constructor() {
     let $ = document.querySelector.bind(document);
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
     this._listaNegociacoes = new ListaNegociacoes();
+
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-
     this._negociacoesView.update(this._listaNegociacoes);
-  }
-  adiciona(event) {
 
+    this._mensagem = new Mensagem();
+    this._mensagemView = new MensagemView($('#mensagemView'));
+    this._mensagemView.update(this._mensagem);
+  }
+
+  adiciona(event) {
     event.preventDefault();
     this._listaNegociacoes.adiciona(this._criaNegociacao());
     this._negociacoesView.update(this._listaNegociacoes);
+
+    this._mensagem.texto = 'Negociação adicionada com sucesso!';
+    this._mensagemView.update(this._mensagem);
+
     this._limpaFormulario();
   }
 
   _criaNegociacao() {
-
     return new Negociacao(
       DateHelper.textoParaData(this._inputData.value),
       this._inputQuantidade.value,
@@ -28,7 +34,6 @@ class NegociacaoController {
   }
 
   _limpaFormulario() {
-
     this._inputData.value = '';
     this._inputQuantidade.value = 1;
     this._inputValor.value = 0.0;
